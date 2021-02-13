926790d08990b9bfe514edf98fe54bb7bc504223
Hector Fernandez <hkfernandezdev@gmail.com>
2021-02-13 17:27:39 -0600

    stock-api-routes.js - basic buy route completed - yet to be tested

--- routes/stock-api-routes.js
+++ routes/stock-api-routes.js
@@ -1,39 +1,54 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/authors", function(req, res) {
-    // 1. Add a join to include all of each Author's Posts
-    // db.Author.findAll({}).then(function(dbAuthor) {
-    //   res.json(dbAuthor);
-    // });
-  });
+	app.get("/api/authors", function(req, res) {
+		// 1. Add a join to include all of each Author's Posts
+		// db.Author.findAll({}).then(function(dbAuthor) {
+		//   res.json(dbAuthor);
+		// });
+	});
 
-  app.post("/api/buy", function(req, res) {
-	let transactionData = {
-		fkUserId: body.req.userId,
-		fkStockId: body.req.stockId,
-		qtyPurchased: body.req.qty,
-		dateSold: "",
-		purchaseDateValue: ""
-	}
-	db.Stock.findAll(
-		{
-			where: {
-				id: req.stock.id
+	app.post("/api/buy", 
+		function(req, res) {
+			let qtyPurchased = body.req.qty
+			let transactionData = {									//an oject to create the transaction row
+				fkUserId: body.req.userId,
+				fkStockId: body.req.stockId,
+				qtyPurchased: body.req.qty,
+				dateSold: "",
+				purchasePrice: "",
+				totalValue: ""
 			}
-		}
-	).then ( 
-		(response) => {
-			transactionData.purchaseDateValue = response.value;		//what is the column name in the stock table?
-			db.Transaction.create(transactionData)
-			.then(
-				function(transaction) {
-					res.json(transaction);
+			db.Stock.findAll(										//finds the stock to be purchased
+				{
+					where: {
+						id: req.stock.id
+					}
+				}
+			).then ( 												//sets transaction purchase price to current stock price
+				( { price } ) => {
+					transactionData.purchaseDateValue = price;		//?????????????what is the column name in the stock table?
+					transactionData.totalValue = response.price * transactionData.qtyPurchased;
+					return transactionData;
+				}
+			).then (												//creates the transaction
+				(transactionData) => {
+					db.Transaction.create(transactionData);
+				}
+			).then (												//returns the transaction creation response
+				() => { 
+					res.json(transactionData);
+					return transactionData }
+			).then (												//finds the user and decrements their balance by the transaction total value
+				( { fkUserId, totalValue } ) => {
+					db.Users.update(
+						{ currentBalance: users.currentBalance - totalValue },  //??????????how to add to an existing value
+						{ where: { id: fkUserId } }
+					)
 				}
 			)
 		}
-	)		
-}
+	)
 
   app.post("/api/authors", function(req, res) {
     // db.Author.create(req.body).then(function(dbAuthor) {
@@ -50,5 +65,9 @@ module.exports = function(app) {
     //   res.json(dbAuthor);
     // });
   });
+
+  axios.get('/user?ID=12345')
+    .then(function (response) {
+	  console.log(response);
 
 };
