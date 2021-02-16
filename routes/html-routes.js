d0d57e9556aa49ea57bf91108791ff3e71d1c31c
Hector Fernandez <hkfernandezdev@gmail.com>
2021-02-16 16:40:05 -0600

    html-routes.js - updated dashboard route to provide a summary of transactions based on stockId foreign key / transaction-block.handlebars - updated block

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -30,9 +30,11 @@ module.exports = function(app) {
 			{ 
 				where: { id: [req.params.id] },
 				include: [ 
-					{ model: db.Transaction, 
-						include: [ { model: db.Stock } ] 
-					} 
+					{ 
+						model: db.Transaction, 
+						include: [ { model: db.Stock } ],
+						group: [ db.Transaction.StockId ]
+					}
 				]
 			}
 		).then ( 
@@ -41,19 +43,61 @@ module.exports = function(app) {
 				let transactionsArr = userObj[0].Transactions;
 				console.log('UNPACKED RESPONSE', unpack(response));
 				console.log('TRANSACTIONS', transactionsArr);
-				console.log('response ', response);
+				// console.log('response ', response);
+				let summaryArr = [];
+				let position = -1;
+		
+				for (let i = 0; i < transactionsArr.length; i++) {
+					if (position === -1 || 
+					transactionsArr[i].Stock.id !== summaryArr[position].id) {
+						// console.log('POSITION ', position);
+						// console.log('SUMMARY TRANSACTION AT CURRENT POSTIION ', summaryArr[position]);
+						// console.log('TRANSACTION ARRAY STOCK ID ',transactionsArr[i].Stock.id);
+						// if (position !== -1){
+						// 	console.log('SUMMARY TRANSACTION ID', summaryArr[position].id);
+						// }
+						let transactionObj = {};
+						transactionObj.id = transactionsArr[i].Stock.id;
+						transactionObj.name = transactionsArr[i].Stock.name;
+						transactionObj.symbol = transactionsArr[i].Stock.symbol;
+						transactionObj.price = transactionsArr[i].Stock.price;
+						transactionObj.qty = 0;
+						transactionsArr.forEach (
+							transaction => {
+								if (transaction.Stock.name === transactionObj.name) {
+									transactionObj.qty += transaction.qty
+								}
+							}
+						);
+						transactionObj.totalVal = transactionObj.qty * transactionObj.price;
+						summaryArr.push (transactionObj);
+						position ++;
+						// console.log('POSITION AT THE END',position);
+	
+					
+						// console.log('SUMMARY ARRAY STOCK NAME ',summaryArr[position].name);
+
+					}
+					console.log('SUMMARY ARRAY ',summaryArr);
+				}
+				return {
+					summaryTransactions: summaryArr,
+					userInfo: userObj
+				}
+			}
+		).then ( 
+			(result) => {
 				res.render ( "dashboard", 
 					{ 
-						user: userObj,
-						transactions: transactionsArr
+						user: result.userInfo,
+						transactions: result.summaryTransactions
 					}
-					// { user: unpack(response) }
 				)
+				console.log('RESULTS', result.userInfo, result.summaryTransactions);
 			}
-		)
+  		)
 	}
-  )
-
+)
 
 
   app.get("/transaction", (req, res) =>{
