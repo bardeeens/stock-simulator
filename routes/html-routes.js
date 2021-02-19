c956d6f430c9172e0f30894004d6bb6a7c083fa6
Hector Fernandez <hkfernandezdev@gmail.com>
2021-02-18 20:45:16 -0600

    commit before merge

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -78,7 +78,7 @@ module.exports = function(app) {
 				for (let i = 0; i < summaryArr.length; i++) {
 					totStockVal += summaryArr[i].qty * summaryArr[i].price;
 				}
-				let netWorth = totStockVal + parseInt(userObj[0].currentBalance)
+				let netWorth = totStockVal + parsefloat(userObj[0].currentBalance)
 				// console.log('USER CURRENT BALANCE ', userObj[0].currentBalance);
 				// console.log('TOTAL STOCK VALUE ', totStockVal);
 				console.log('USER OBJECT !!!!!!!!!!!!!!!!!!!!!!!!!',userObj);
